ec984320371e8baa73a7f73a8a14a5166c633e18
crr0004 <crr0004@gmail.com>
2015-07-31 19:47:03 +1000

    Added global git config. Added to copyme

--- copyme.sh
+++ copyme.sh
@@ -6,7 +6,7 @@ then
 	cp .bashrc $1
 	cp .vimrc $1
 	cp dirtags $1
-	cp .gitconfig $1
+	cp .gitconfig_global $1
 	cp .tmux.conf $1
 else
 	echo "Must supply a directory to copy to. Suggest home directory of ~/"
