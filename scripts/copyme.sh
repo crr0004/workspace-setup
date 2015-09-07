9ddfee3a7bd50b277f3c349993306c7cc9694fe8
crr0004 <crr0004@gmail.com>
2015-09-07 21:10:30 +1000

    Edited copyme.sh to reflect new directory structure

--- scripts/copyme.sh
+++ scripts/copyme.sh
@@ -3,13 +3,13 @@
 #$1 should be dir to copy to
 if [[ -n "$1" ]] && [ -d $1 ];
 then
-	cp .bashrc $1
-	cp .vimrc $1
+	cp ../.bashrc $1
+	cp ../.vimrc $1
 	cp dirtags $1
-	cp .gitconfig_global $1/.gitconfig
-	cp .tmux.conf $1
-	cp .ctags $1
-	cp .ycm_extra_conf.py $1
+	cp ../.gitconfig_global $1/.gitconfig
+	cp ../.tmux.conf $1
+	cp ../.ctags $1
+	cp ../.ycm_extra_conf.py $1
 else
 	echo "Must supply a directory to copy to. Suggest home directory of ~/"
 fi
