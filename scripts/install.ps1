72f11cadff4df4717bd20649db5d917a76e8bf5e
Chris Rhodes <crr0004@gmail.com>
2019-03-09 14:38:08 +1100

    Configures git global ignore

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -25,6 +25,8 @@ checkAndCreate("$HOME\dev\vim\")
 checkAndCreate("$HOME\dev\ctags")
 
 Copy-Item -Path $filesDir\vimrc -Destination $vimHome
+Copy-Item -Path $filesDir\gitignore -Destination $HOME
+git config --global core.excludesfile '~/gitignore'
 start-bitstransfer -destination "$vimHome\autoload\plug.vim" -source https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 start-bitstransfer -destination "$vimHome\colors\molokai.vim" -source https://raw.githubusercontent.com/crr0004/molokai/master/colors/molokai.vim
 Invoke-WebRequest -OutFile ".\temp\ctags-6f8535e-x64.zip" -Uri https://github.com/crr0004/ctags/releases/download/0.1/ctags-6f8535e-x64.zip -Method Get
